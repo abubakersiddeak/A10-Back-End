c695a1f11c55305b5ad8bbce8389ab7427f6c637
abubakersiddeak <abubaersiddeak@gmail.com>
2025-11-09 00:42:40 +0600

    project setup done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("welcome Backend");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+dotenv.config();
+import { MongoClient, ServerApiVersion } from "mongodb";
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // create database
+    const myDB = client.db("myDB");
+    const myColl = myDB.collection("");
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    console.log("successfully connected  mongodb ");
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
