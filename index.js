20ab84e3a7367e6601f920f0b1c41dd0726dba97
abubakersiddeak <abubaersiddeak@gmail.com>
2025-11-10 12:21:27 +0600

    add user create route

--- index.js
+++ index.js
@@ -2,14 +2,39 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import admin from "firebase-admin";
+import serviceAccount from "./firebase-adminsdk.json"with { type: "json" };
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+// firebase 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 // --- Middleware ---
 app.use(cors());
 app.use(express.json());
+
+//  Middleware for Firebase Token Verification
+async function verifyFirebaseToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized - Missing Token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.user = decoded; // user info stored in request
+    next();
+  } catch (error) {
+    console.error("Token verification failed:", error);
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+}
 
 // --- MongoDB Connection ---
 const uri = process.env.MONGODB_URI;
@@ -27,13 +52,47 @@ async function run() {
     const db = client.db("ecoTrackDB");
 
     // Collections
+    const users = db.collection("users");
     const challenges = db.collection("challenges");
     const userChallenges = db.collection("userChallenges");
     const tips = db.collection("tips");
     const events = db.collection("events");
 
     console.log(" MongoDB connected successfully!");
+    
+    app.post("/api/user", verifyFirebaseToken, async (req, res) => {
+  const { name, email } = req.body;
+  const firebaseEmail = req.user.email;
+  if (email !== firebaseEmail) {
+    return res.status(403).json({ message: "Email mismatch" });
+  }
 
+  try {
+    const existing = await users.findOne({ email });
+
+    if (existing) {
+      // update user lastLogin or name
+      await users.updateOne(
+        { email },
+        { $set: { name, lastLogin: new Date() } }
+      );
+      return res.status(200).json({ message: "User updated" });
+    }
+
+    // create new user
+    await users.insertOne({
+      name,
+      email,
+      createdAt: new Date(),
+      lastLogin: new Date(),
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error creating user" });
+  }
+       });
     // Get all challenges
     app.get("/api/challenges", async (req, res) => {
       const filter = req.query.category ? { category: req.query.category } : {};
@@ -53,6 +112,7 @@ async function run() {
       const data = req.body;
       data.createdAt = new Date();
       data.updatedAt = new Date();
+      data.startDate = new Date();
       const result = await challenges.insertOne(data);
       res.json(result);
     });
